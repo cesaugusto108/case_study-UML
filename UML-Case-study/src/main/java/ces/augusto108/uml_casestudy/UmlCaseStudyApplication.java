ac65903e19f7b288263888c5ee217e6d8c303d35
César Augusto <ces.augusto108@gmail.com>
2022-11-24 15:14:10 -0300

    Projeto criado

---
+++ UML-Case-study/src/main/java/ces/augusto108/uml_casestudy/UmlCaseStudyApplication.java
@@ -0,0 +1,11 @@
+package ces.augusto108.uml_casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlCaseStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UmlCaseStudyApplication.class, args);
+    }
+}
---
+++ UML-Case-study/src/test/java/ces/augusto108/uml_casestudy/UmlCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ces.augusto108.uml_casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
