2973c0d2c07ac84bc1e721d9cf4e8f2af3fd1cb1
César Augusto <ces.augusto108@gmail.com>
2022-11-25 09:26:53 -0300

    Classes State e City e repositórios

--- UML-Case-study/src/main/java/ces/augusto108/uml_casestudy/UmlCaseStudyApplication.java
+++ UML-Case-study/src/main/java/ces/augusto108/uml_casestudy/UmlCaseStudyApplication.java
@@ -1,9 +1,13 @@
 package ces.augusto108.uml_casestudy;
 
 import ces.augusto108.uml_casestudy.domain.entities.Category;
+import ces.augusto108.uml_casestudy.domain.entities.City;
 import ces.augusto108.uml_casestudy.domain.entities.Product;
+import ces.augusto108.uml_casestudy.domain.entities.State;
 import ces.augusto108.uml_casestudy.repositories.CategoryRepository;
+import ces.augusto108.uml_casestudy.repositories.CityRepository;
 import ces.augusto108.uml_casestudy.repositories.ProductRepository;
+import ces.augusto108.uml_casestudy.repositories.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,6 +28,12 @@ public class UmlCaseStudyApplication implements CommandLineRunner {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    StateRepository stateRepository;
+
+    @Autowired
+    CityRepository cityRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Category category1 = new Category(null, "Computers");
@@ -43,5 +53,20 @@ public class UmlCaseStudyApplication implements CommandLineRunner {
 
         categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
         productRepository.saveAll(Arrays.asList(product1, product2, product3));
+
+        //
+
+        State state1 = new State(null, "São Paulo");
+        State state2 = new State(null, "Minas Gerais");
+
+        City city1 = new City(null, "São Paulo", state1);
+        City city2 = new City(null, "Santos", state1);
+        City city3 = new City(null, "Uberlândia", state2);
+
+        state1.getCities().addAll(Arrays.asList(city1, city2));
+        state2.getCities().add(city3);
+
+        stateRepository.saveAll(Arrays.asList(state1, state2));
+        cityRepository.saveAll(Arrays.asList(city1, city2, city3));
     }
 }
